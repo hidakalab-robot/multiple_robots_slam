a174df151cced2a33a1d22b9b6856947204cd344
Yuki-Narita <14ej079@ms.dendai.ac.jp>
2018-04-23 20:34:59 +0900

    point_cloud cluster

--- new_exploration_programs/src/test_code/analysis_point_cloud.cpp
+++ new_exploration_programs/src/test_code/analysis_point_cloud.cpp
@@ -1,64 +1,51 @@
 #include <ros/ros.h>
 #include <ros/callback_queue.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
 
-#include <pcl/ModelCoefficients.h>  
-//#include <pcl/point_types.h>  
-#include <pcl/io/pcd_io.h>  
-#include <pcl/features/normal_3d.h>  
-#include <pcl/filters/extract_indices.h>  
-//#include <pcl/filters/voxel_grid.h>  
-#include <pcl/kdtree/kdtree.h>  
-#include <pcl/sample_consensus/method_types.h>  
-#include <pcl/sample_consensus/model_types.h>  
-#include <pcl/segmentation/sac_segmentation.h>  
-#include <pcl/segmentation/extract_clusters.h>  
-//#include <pcl/visualization/cloud_viewer.h>
+
+/*用途不明ヘッダー*/
+//#include <pcl_conversions/pcl_conversions.h>
+//#include <pcl/point_cloud.h>
+//#include <pcl/point_types.h>
+//#include <pcl/io/pcd_io.h>
+//#include <pcl/ModelCoefficients.h>
+//#include <pcl/features/normal_3d.h>
+//#include <pcl/kdtree/kdtree.h>
+//#include <pcl/sample_consensus/method_types.h>
+//#include <pcl/sample_consensus/model_types.h>
+
 
 
 class AnalysisPointCloud
 {
 private:
-	ros::NodeHandle apc;	
-	ros::NodeHandle apc2;
+	ros::NodeHandle apcs;
 	ros::NodeHandle apcp;
+
 	ros::Subscriber pc_sub;
-	ros::Subscriber pc_sub2;
 
-	ros::Publisher pc_pub;
+	ros::Publisher pc_pub1;
 	ros::Publisher pc_pub2;
+	ros::Publisher pc_pub3;
+	ros::Publisher pc_pub4;
 
-	uint32_t height;
-	uint32_t width;
-	std::vector<sensor_msgs::PointField> fields;
-	bool is_bigendian;
-	uint32_t  point_step;
-	uint32_t  row_step;
-	std::vector<uint8_t> data;
-	bool is_dense;
-	std::string name;
-	uint32_t offset;
-	uint8_t  datatype;
-	uint32_t count;
 public:
 	ros::CallbackQueue pc_queue;
-	ros::CallbackQueue pc_queue2;
 	AnalysisPointCloud()
 	{
-		apc.setCallbackQueue(&pc_queue);
-		pc_sub = apc.subscribe("/camera/depth_registered/points",1,&AnalysisPointCloud::pointcloud_callback,this);
-		apc2.setCallbackQueue(&pc_queue2);
-		pc_sub2 = apc2.subscribe("/camera/depth_registered/points",1,&AnalysisPointCloud::processing_pc,this);
-		pc_pub = apcp.advertise<sensor_msgs::PointCloud2>("edit_cloud", 1);
+		apcs.setCallbackQueue(&pc_queue);
+		pc_sub = apcs.subscribe("/camera/depth_registered/points",1,&AnalysisPointCloud::processing_pc,this);
+		pc_pub1 = apcp.advertise<sensor_msgs::PointCloud2>("edit_cloud1", 1);
 		pc_pub2 = apcp.advertise<sensor_msgs::PointCloud2>("edit_cloud2", 1);
+		pc_pub3 = apcp.advertise<sensor_msgs::PointCloud2>("edit_cloud3", 1);
+		pc_pub4 = apcp.advertise<sensor_msgs::PointCloud2>("edit_cloud4", 1);
 	};
 	~AnalysisPointCloud(){};
-	void pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr& pc_msg);
 	void processing_pc(const sensor_msgs::PointCloud2::ConstPtr& ppc_msg);
 };
 
@@ -66,73 +53,139 @@ public:
 void AnalysisPointCloud::processing_pc(const sensor_msgs::PointCloud2::ConstPtr& ppc_msg)
 {
 	std::cout << "1" << std::endl;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr test_cloud;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr test_cloud (new pcl::PointCloud<pcl::PointXYZ>);
 	std::cout << "2" << std::endl;
 	pcl::fromROSMsg (*ppc_msg, *test_cloud);
 	std::cout << "get_point_cloud" << std::endl;
 
+/*初期状態を出力*/
+	sensor_msgs::PointCloud2 edit_cloud1;
+	pcl::toROSMsg (*test_cloud, edit_cloud1);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud;
-	pcl::VoxelGrid<pcl::PointXYZ> vgf;
-	vgf.setInputCloud (test_cloud);
-	vgf.setLeafSize (0.1f, 0.1f, 0.1f);
-	vgf.filter (*filtered_cloud);
-	
 	for(int i=0;i<test_cloud->points.size();i++)
 	{
-		test_cloud->points[i].x+=1.0;
-		//std::cout << test_cloud.points[i].x << ", " << test_cloud.points[i].y << ", " << test_cloud.points[i].z << std::endl;
-	//", " << +test_cloud.points[i].r << ", " << +test_cloud.points[i].g << ", " << +test_cloud.points[i].b << std::endl;
-		if(!ros::ok())
-			break;
+		test_cloud->points[i].x+=3.0;
 	}
-	std::cout << "edit_cloud" << std::endl;
-	sensor_msgs::PointCloud2 edit_cloud, edit_cloud2;
-	pcl::toROSMsg (*test_cloud, edit_cloud);
-	pcl::toROSMsg (*filtered_cloud, edit_cloud2);
 
-	pc_pub.publish(edit_cloud);
+/*voxel_grid処理*/
+	pcl::PointCloud<pcl::PointXYZ>::Ptr voxeled_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::VoxelGrid<pcl::PointXYZ> vg;
+	vg.setInputCloud (test_cloud);
+	vg.setLeafSize (0.01f, 0.01f, 0.01f);
+	vg.filter (*voxeled_cloud);
+
+/*voxel_grid時点での出力*/
+	sensor_msgs::PointCloud2 edit_cloud2;
+	pcl::toROSMsg (*voxeled_cloud, edit_cloud2);
+
+	for(int i=0;i<voxeled_cloud->points.size();i++)
+	{
+		voxeled_cloud->points[i].x+=3.0;
+	}
+
+/*論文ではここで床面除去を入れてるけど、取り敢えず平面除去*/
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::SACSegmentation<pcl::PointXYZ> seg;
+	pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+	seg.setOptimizeCoefficients (true);
+	seg.setModelType (pcl::SACMODEL_PLANE);
+	seg.setMethodType (pcl::SAC_RANSAC);
+	seg.setMaxIterations (100);
+	seg.setDistanceThreshold (0.02);
+
+	int i=0;
+	int nr_points = (int) voxeled_cloud->points.size ();
+
+	while (voxeled_cloud->points.size () > 0.3 * nr_points)
+	{
+	 seg.setInputCloud (voxeled_cloud);
+	 seg.segment (*inliers, *coefficients);
+	 if (inliers->indices.size () == 0)
+	 {
+		 std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+		 break;
+	 }
+
+	 pcl::ExtractIndices<pcl::PointXYZ> extract;
+	 extract.setInputCloud (voxeled_cloud);
+	 extract.setIndices (inliers);
+
+	 extract.setNegative (true);
+	 extract.filter (*cloud_f);
+	 *voxeled_cloud = *cloud_f;
+ }
+
+
+/*平面除去出力*/
+
+	sensor_msgs::PointCloud2 edit_cloud3;
+	pcl::toROSMsg (*voxeled_cloud, edit_cloud3);
+
+	for(int i=0;i<voxeled_cloud->points.size();i++)
+	{
+		voxeled_cloud->points[i].x+=3.0;
+	}
+
+
+/*ユークリッドクラスタリング処理*/
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+	tree->setInputCloud (voxeled_cloud);
+	std::vector<pcl::PointIndices> cluster_indices;
+	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+	ec.setClusterTolerance (0.08);
+	ec.setMinClusterSize (100);
+	ec.setMaxClusterSize (25000000);
+	ec.setSearchMethod (tree);
+	ec.setInputCloud (voxeled_cloud);
+	ec.extract (cluster_indices);
+
+	int j = 0;
+	float colors[6][3] ={{255, 0, 0}, {0,255,0}, {0,0,255}, {255,255,0}, {0,255,255}, {255,0,255}};
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cluster_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::copyPointCloud(*voxeled_cloud, *cluster_cloud);
+
+	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+  {
+      for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
+			{
+					cluster_cloud->points[*pit].r = colors[j%6][0];
+					cluster_cloud->points[*pit].g = colors[j%6][1];
+					cluster_cloud->points[*pit].b = colors[j%6][2];
+      }
+      j++;
+  }
+
+/*ユークリッドクラスタリング出力*/
+	sensor_msgs::PointCloud2 edit_cloud4;
+	pcl::toROSMsg (*cluster_cloud, edit_cloud4);
+
+	std::cout << "edit_cloud" << std::endl;
+
+	pc_pub1.publish(edit_cloud1);
 	pc_pub2.publish(edit_cloud2);
+	pc_pub3.publish(edit_cloud3);
+	pc_pub4.publish(edit_cloud4);
 	std::cout << "publish_cloud" << std::endl;
 
-}
-
-
-
-void AnalysisPointCloud::pointcloud_callback(const sensor_msgs::PointCloud2::ConstPtr& pc_msg)
-{
-	height = pc_msg -> height;
-	width = pc_msg -> width;
-	fields = pc_msg -> fields;
-	is_bigendian = pc_msg -> is_bigendian;
-	point_step = pc_msg -> point_step;
-	row_step = pc_msg -> row_step;
-	data = pc_msg -> data;
-	is_dense = pc_msg -> is_dense;
-
-	std::cout << "height: " << height << "\n" << "width: " << width << "\n" << "is_bigendian: " << is_bigendian <<  "\n" << "point_step: " << point_step << "\n" << "row_step: " << row_step << "\n" << "data_size: " << data.size() << "\n" << "is_dense: " << is_dense << std::endl;
-
-
-	for(int i=0;i<fields.size();i++)
+/*以下ごみ*/
+	/*for(int i=0;i<test_cloud->points.size();i++)
 	{
-		name = fields[i].name;
-		offset = fields[i].offset;
-		datatype = fields[i].datatype;
-		count = fields[i].count;
-		//std::cout << "name: " << name << ", " << "offset: " << offset << ", " << "datatype: " << +datatype <<  ", " << "count: " << count << std::endl;
-		//std::cout << "name: " << name << std::endl;
-		//std::cout << "offset: " << offset << std::endl; 
-		//std::cout << "datatype: " << datatype << std::endl; 
-		//std::cout << "count: " << count << std::endl; 
-	}
-
-	
-	for(int i=0;i<100;i++)
-	{
-		std::cout << "data[" << i << "]: " << +data[i] << std::endl;
+		test_cloud->points[i].x+=3.0;
+		std::cout << test_cloud.points[i].x << ", " << test_cloud.points[i].y << ", " << test_cloud.points[i].z << std::endl;
+	", " << +test_cloud.points[i].r << ", " << +test_cloud.points[i].g << ", " << +test_cloud.points[i].b << std::endl;
 		if(!ros::ok())
 			break;
-	}
+	}*/
+
+	/*for(int i=0;i<voxeled_cloud->points.size();i++)
+	{
+		std::cout << filtered_cloud->width  << '\n';
+		std::cout << filtered_cloud->height  << '\n';
+		std::cout << voxeled_cloud->points[i].x << ", " << voxeled_cloud->points[i].y << ", " << voxeled_cloud->points[i].z << std::endl;
+	", " << +test_cloud.points[i].r << ", " << +test_cloud.points[i].g << ", " << +test_cloud.points[i].b << std::endl;
+}*/
 }
 
 
@@ -141,9 +194,8 @@ int main(int argc, char** argv)
 	ros::init(argc, argv, "analysis_point_cloud");
 	AnalysisPointCloud apc;
 	while(ros::ok()){
-		//apc.pc_queue.callOne(ros::WallDuration(1));//使用しない関数
 		std::cout << "0" << std::endl;
-		apc.pc_queue2.callOne(ros::WallDuration(10));
+		apc.pc_queue.callOne(ros::WallDuration(1));
 	}
 	return 0;
 }
