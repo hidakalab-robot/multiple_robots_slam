237b20df084b4d6df85597cf8ad416a6e8067afd
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-08-05 19:17:06 +0900

    fix corrdinateConverter

--- exploration_libraly/include/exploration_libraly/utility.hpp
+++ exploration_libraly/include/exploration_libraly/utility.hpp
@@ -19,10 +19,12 @@ template<> void coordinateConverter(const tf::TransformListener& l, const std::s
     double transX = transform.getOrigin().getX();
     double transY = transform.getOrigin().getY();
 
+    // ROS_INFO_STREAM("trans : " << transX << ", " << transY << ", " << transYaw);
+
     Eigen::Matrix2d rotation;
     rotation << cos(transYaw),-sin(transYaw),sin(transYaw),cos(transYaw);
 
-    Eigen::Vector2d tempPoint(rotation * Eigen::Vector2d(p.position.x - transX, p.position.y - transY));
+    Eigen::Vector2d tempPoint(rotation * Eigen::Vector2d(p.position.x, p.position.y) + Eigen::Vector2d(transX, transY));
     p.position = msgPoint(tempPoint.x(),tempPoint.y());
     p.orientation = tfQuaToGeoQua(tf::Quaternion(p.orientation.x,p.orientation.y,p.orientation.z,p.orientation.w)*=transQ);
 }
@@ -33,6 +35,7 @@ template<> geometry_msgs::Pose coordinateConverter(const tf::TransformListener& 
 }
 
 template <typename T> T coordinateConverter(const tf::TransformListener& l, const std::string& destFrame, const std::string& origFrame, geometry_msgs::Point& p);
+template <typename T> T coordinateConverter(const tf::TransformListener& l, const std::string& destFrame, const std::string& origFrame, const geometry_msgs::Point& p);
 
 template<> void coordinateConverter(const tf::TransformListener& l, const std::string& destFrame, const std::string& origFrame, geometry_msgs::Point& p){
     geometry_msgs::Pose ps = pointToPose(p);
@@ -40,10 +43,14 @@ template<> void coordinateConverter(const tf::TransformListener& l, const std::s
     p = ps.position;
 }
 
-template<> geometry_msgs::Point coordinateConverter(const tf::TransformListener& l, const std::string& destFrame, const std::string& origFrame, geometry_msgs::Point& p){
-    coordinateConverter<void>(l,destFrame,origFrame,p);
-    return p;
+template<> geometry_msgs::Point coordinateConverter(const tf::TransformListener& l, const std::string& destFrame, const std::string& origFrame, const geometry_msgs::Point& p){
+    // ROS_INFO_STREAM("input : " << p.x << ", " << p.y);
+    geometry_msgs::Point pl = p;
+    coordinateConverter<void>(l,destFrame,origFrame,pl);
+    // ROS_INFO_STREAM("output : " << pl.x << ", " << pl.y);
+    return pl;
 }
+
 }
 
 #endif // UTILITY_HPP
