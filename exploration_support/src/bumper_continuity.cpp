d833741847c51ce13c166949ac4c4a3eeb1ddd24
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-09-13 18:03:19 +0900

    add map_continuity

--- exploration_support/src/bumper_continuity.cpp
+++ exploration_support/src/bumper_continuity.cpp
@@ -2,7 +2,7 @@
 #include <kobuki_msgs/BumperEvent.h>
 
 int main(int argc, char *argv[]){
-    ros::init(argc, argv, "continuity");
+    ros::init(argc, argv, "bumper_continuity");
     Continuity<kobuki_msgs::BumperEvent> cbe("mobile_base/events/bumper", "bumper");
     while(ros::ok()) cbe.publish();    
     return 0;
