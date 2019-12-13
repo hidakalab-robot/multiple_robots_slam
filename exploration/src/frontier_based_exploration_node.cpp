2a849cd64aff633a0b8b5304d53976388f51b291
shuinoue <shuinoue0817@gmail.com>
2019-12-13 23:06:58 +0900

    adjustment for simulation SI

--- exploration/src/frontier_based_exploration_node.cpp
+++ exploration/src/frontier_based_exploration_node.cpp
@@ -2,6 +2,7 @@
 #include <exploration/frontier_based_exploration.h>
 #include <exploration/movement.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Int8.h>
 #include <exploration_libraly/struct.h>
 #include <geometry_msgs/PointStamped.h>
 
@@ -13,6 +14,7 @@ int main(int argc, char *argv[]){
     Movement mv;
 
     ExpLib::Struct::subStruct<std_msgs::Bool> end("end",1);
+    ExpLib::Struct::pubStruct<std_msgs::Int8> checkpub("/timing_check", 1);
     geometry_msgs::PointStamped goal;
 
     ros::NodeHandle p("~");
@@ -23,7 +25,12 @@ int main(int argc, char *argv[]){
 
     usleep(2e5);//timeがsim_timeに合うのを待つ
     ros::Time start = ros::Time::now();
-
+    std_msgs::Int8 timingStatus;
+    timingStatus.data = 1;
+    checkpub.pub.publish(timingStatus);
+    sleep(1);
+    timingStatus.data = 2;
+    checkpub.pub.publish(timingStatus);
     if(!DEBUG && ROTATION) mv.oneRotation();
 
     while(ros::ok()){
@@ -33,7 +40,8 @@ int main(int argc, char *argv[]){
     }
 
     ROS_INFO_STREAM("exploration finish !! -> time : " << ros::Duration(ros::Time::now()-start).toSec() << " [s]");
-
+    timingStatus.data = 3;
+    checkpub.pub.publish(timingStatus);
     ros::shutdown();
     
     return 0;
