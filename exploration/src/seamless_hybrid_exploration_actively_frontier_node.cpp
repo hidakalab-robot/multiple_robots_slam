31e1d8ec2241ef7f4fa1c1c4d796c8a30efa30a5
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-12-21 19:55:39 +0900

    add SHEAF

--- exploration/src/seamless_hybrid_exploration_actively_frontier_node.cpp
+++ exploration/src/seamless_hybrid_exploration_actively_frontier_node.cpp
@@ -1,22 +1,26 @@
 #include <ros/ros.h>
 #include <exploration/seamless_hybrid_exploration.h>
+#include <exploration/frontier_based_exploration.h>
 #include <exploration/movement.h>
 #include <std_msgs/Bool.h>
 #include <exploration_libraly/struct.h>
 #include <geometry_msgs/PointStamped.h>
 
 int main(int argc, char* argv[]){
-    ros::init(argc, argv, "seamless_hybrid_exploration");
+    ros::init(argc, argv, "seamless_hybrid_exploration_actively_frontier");
 
     SeamlessHybridExploration she;
+    FrontierBasedExploration fbe;
     Movement mv;
 
     ExpLib::Struct::subStruct<std_msgs::Bool> end("end",1);
+    ExpLib::Struct::subStruct<std_msgs::Bool> areaDiff("end/area_diff",1);
 
     geometry_msgs::PointStamped goal;
     ros::NodeHandle p("~");
     bool DEBUG,ROTATION,AUTO_FINISH;
     double BRANCH_WAIT_TIME;
+    double AREA_DIFF_THREDHOLD;
     p.param<bool>("debug",DEBUG,false);
     p.param<bool>("rotation",ROTATION,true);
     p.param<bool>("auto_finish",AUTO_FINISH,true);
@@ -42,12 +46,23 @@ int main(int argc, char* argv[]){
     if(!DEBUG && ROTATION) mv.oneRotation();
 
     while(ros::ok()){
+        if(!areaDiff.q.callOne(ros::WallDuration(0.5)) && areaDiff.data.data) break;// ここに切り替え条件入れる
         branchTimer() && she.getGoal(goal) && !DEBUG ? mv.moveToGoal(goal) : mv.moveToForward();
         if(AUTO_FINISH && !end.q.callOne(ros::WallDuration(0.5)) && end.data.data) break;
         ros::spinOnce();
     }
 
+    ros::Time switchTime = ros::Time::now();
+    ROS_INFO_STREAM("Switch exploration method SHE to FBE -> switch time :" << ros::Duration(switchTime-start).toSec() << " [s]");
+
+    while(ros::ok()){
+        if(fbe.getGoal(goal) && !DEBUG) mv.moveToGoal(goal);
+        if(AUTO_FINISH && !end.q.callOne(ros::WallDuration(0.5)) && end.data.data) break;
+        ros::spinOnce();
+    }
+
     ROS_INFO_STREAM("exploration finish !! -> time : " << ros::Duration(ros::Time::now()-start).toSec() << " [s]");
+    ROS_INFO_STREAM("switch time :" << ros::Duration(switchTime-start).toSec() << " [s]");
 
     ros::shutdown();
     
